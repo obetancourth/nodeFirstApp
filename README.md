6df10758d5b115f0c6ee1bf3b41763ccc9c42ef6
Orlando Betancourth <orlando.betancourth@gmail.com>
2015-03-10 21:04:50 -0600

    Estructura Basica del Proyecto con MongoDB y JQM

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 #Mi Primer Aplicaciones con Node.JS, MongoDB y Express
+##Notas
+* Para facilitar la ejecucion en tiempo de depuración se debe crear un script que setee las siguientes variables en el ambiente:
+
+**JQM_USECDN=1
+**PORT=80
+**DEBUG=newNodeProject
--- app.js
+++ app.js
@@ -5,57 +5,62 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+function app(db){
+  //var routes = require('./routes/index');
+  //var users = require('./routes/users');
 
-var app = express();
+  var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(__dirname + '/public/favicon.ico'));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(require('less-middleware')(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+  //app.use('/', routes);
+  //app.use('/users', users);
+  app.get("/", function(req, res){
+		var file = path.join(__dirname, 'public/front-end/index.html');
+		res.sendFile(file);
+	});
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  // error handlers
 
-// error handlers
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
+  // production error handler
+  // no stacktraces leaked to user
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: {}
     });
   });
+  return app;
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
