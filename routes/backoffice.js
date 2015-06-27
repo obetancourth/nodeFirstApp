dbfea6796daa872a8f90d463e618fc2ea9ed305e
Orlando Betancourth <orlando.betancourth@gmail.com>
2015-06-27 10:25:10 -0600

    Arreglo de Fechas y Orden de Reportes de Fecha en BakcOffice

--- routes/backoffice.js
+++ routes/backoffice.js
@@ -7,17 +7,28 @@ function backoffice_router(db){
     var secciones = db.collection("secciones");
 
     router.get("/", function(req,res){
-        secciones.find({"Reportes.Status":"Reportado"}).toArray(
+        secciones.find({"Reportes.Status":"Reportado"},{sort:[["FechaReporte",1]]}).toArray(
             function(err, reportes){
+                var finalReport = new Array();
                 for(var i= 0; i<reportes.length; i++){
                     for(var j=0; j<reportes[i].Reportes.length;j++){
-                        console.log(reportes[i].Reportes[j].FechaReporte);
                         var d = new Date(reportes[i].Reportes[j].FechaReporte)
-                        reportes[i].Reportes[j].FechaReporte = d.getDay() + "/" + (d.getMonth() + 1)+ "/" + d.getFullYear();
-                        console.log(reportes[i].Reportes[j].FechaReporte);
+                        d =  d.getDate() + "/" + (d.getMonth() + 1)+ "/" + d.getFullYear();
+                        finalReport.push({  "FechaReporte":d,
+                                            "NombreCurso": reportes[i].NombreCurso,
+                                            "Seccion": reportes[i].Seccion,
+                                            "NombreDocente": reportes[i].NombreDocente,
+                                            "Status": reportes[i].Reportes[j].Status,
+                                            "sortFecha": new Date(reportes[i].Reportes[j].FechaReporte)
+                                        });
                     }
                 }
-                res.render('index',{"title":"Monitoreo de Aulas", "reportes":reportes});
+                finalReport.sort(function(obj1, obj2){
+                    if(obj1.sortFecha.getTime() < obj2.sortFecha.getTime()) return -1;
+                    if(obj1.sortFecha.getTime() > obj2.sortFecha.getTime()) return 1;
+                    return 0;
+                });
+                res.render('index',{"title":"Monitoreo de Aulas", "reportes":finalReport});
             }
         );
 
